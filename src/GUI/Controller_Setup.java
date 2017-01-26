fe25c26225fc9a04b13f28c663870fae4a967598
Bekker,David D.S.H. de <david.debekker@student.fontys.nl>
2017-01-26 17:19:32 +0100

    Package setup

--- src/GUI/Controller_Setup.java
+++ src/GUI/Controller_Setup.java
@@ -1,9 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package sonarqubehelper;
+package GUI;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,10 +8,11 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
 /**
- *
+ * FXML Controller class
+ * 
  * @author David
  */
-public class FXMLDocumentController implements Initializable {
+public class Controller_Setup implements Initializable {
     
     @FXML
     private Label label;
---
+++ src/Models/ProcedureTask.java
@@ -0,0 +1,14 @@
+package Models;
+
+/**
+ *
+ * @author David
+ */
+public class ProcedureTask implements Runnable{
+
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
