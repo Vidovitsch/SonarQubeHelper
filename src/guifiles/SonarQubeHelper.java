113308de41716ce8ac429c4aed4dd42d45b40117
Bekker,David D.S.H. de <david.de.bekker@live.nl>
2017-02-01 21:20:23 +0100

    FXML fields organized better

---
+++ src/guifiles/SonarQubeHelper.java
@@ -0,0 +1,292 @@
+package guifiles;
+
+import enums.opsystem;
+import handlers.PropertyHandler;
+import models.ScannerTask;
+import java.io.File;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
+import models.StartSQServer;
+
+/**
+ *
+ * @author David
+ */
+public class SonarQubeHelper extends Application {
+    
+    private Stage stage;
+    private ControllerMain mainController;
+    private PropertyHandler pHandler;
+    private Thread procedure;
+    private Alert info;
+    private final static int portNumber = 9000;
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        this.stage = stage;
+        this.pHandler = new PropertyHandler();
+        if (pHandler.checkForPropertyFile()) {
+            openMainScreen();
+        } else {
+            pHandler.createPropertyFile();
+            openSetupScreen();
+        }
+    }
+
+    @Override
+    public void stop() {
+        //Calling stop instead of interrupt; interrupting won't stop running cmd processes, while stop does.
+        procedure.stop();
+    }
+    
+    /**
+     * Opens the setup screen.
+     * This method only gets called when the button 'settings' has been pressed
+     * or when it's the first time this application gets started.
+     * 
+     * @throws IOException 
+     */
+    public void openSetupScreen() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("FXMLSetup.fxml"));
+        Parent root = (Parent) loader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Setup");
+        stage.show();
+        ControllerSetup setupController = (ControllerSetup) loader.getController();
+        setupController.setSQHelper(this);
+        setupController.setValues(pHandler.getSQRootsFromPropertyFile(), pHandler.getSystemFromPropertyFile(),
+                pHandler.getSourceFromPropertyFile());
+    }
+    
+    /**
+     * Opens the main screen.
+     * 
+     * @throws IOException 
+     */
+    public void openMainScreen() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("FXMLMain.fxml"));
+        Parent root = (Parent) loader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Main");
+        stage.show();
+        mainController = (ControllerMain) loader.getController();
+        mainController.setSQHelper(this);
+        mainController.setValues(pHandler.getProjectRootFromPropertyFile());
+    }
+    
+    /**
+     * Opens the file explorer.
+     * 
+     * @param title
+     * @return The file path of the selected file
+     */
+    public String openFileExplorer(String title) {
+        DirectoryChooser chooser = new DirectoryChooser();
+        chooser.setTitle(title);
+        File selectedDirectory = chooser.showDialog(stage);
+        return selectedDirectory.getPath();
+    }
+    
+    /**
+     * Saves the paths set in the setup screen.
+     * 
+     * @param paths 
+     * @param system 
+     * @param src 
+     */
+    public void saveSetup(String[] paths, opsystem system, String src) {
+        try {
+            pHandler.savePropertyFileSQRoots(paths);
+            pHandler.savePropertyFileSystem(system);
+            pHandler.savePropertyFileSource(src);
+        } catch (IOException ex) {
+            Logger.getLogger(SonarQubeHelper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    /**
+     * Starts the scanning of the set project.
+     * First a sonarqube property file is created if it doesn't already exist.
+     * 
+     * @param path 
+     */
+    public void startScanning(String path) {
+        try {
+            pHandler.savePropertyFileRoot(path);
+            pHandler.createSQPropertyFile(path);
+            
+            if (checkPortAvailable()) {
+                (new Thread(new StartSQServer(this, pHandler.getSQRootsFromPropertyFile()[0], path,
+                        pHandler.getSystemFromPropertyFile()))).start();
+            }
+            else {
+                procedure = new Thread(new ScannerTask(this, pHandler.getSQRootsFromPropertyFile()[1],
+                        pHandler.getProjectRootFromPropertyFile()));
+                procedure.start();
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(SonarQubeHelper.class.getName()).log(Level.SEVERE, null, ex);
+            showErrorDialog("Can't scan this project");
+        }
+    }
+    
+    /**
+     * Shows a warning dialog with a set header.
+     * 
+     * @param header 
+     */
+    public void showWarningDialog(String header) {
+        Alert warning = new Alert(AlertType.WARNING);
+        warning.setTitle("Warning Dialog");
+        warning.setHeaderText(null);
+        warning.setContentText(header);
+        warning.showAndWait();
+    }
+    
+    /**
+     * Shows a error dialog with a set header.
+     * 
+     * @param header 
+     */
+    public void showErrorDialog(String header) {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                Alert error = new Alert(AlertType.ERROR);
+                error.setTitle("Error Dialog");
+                error.setHeaderText(null);
+                error.setContentText(header);
+                error.showAndWait();
+
+                //Interrupts running thread
+                procedure.interrupt();
+            }
+        });
+    }
+    
+    /**
+     * Shows a information dialog with a set header.
+     * 
+     * @param header 
+     * @param message 
+     */
+    public void showInfoDialog(String header, String message) {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                info = new Alert(AlertType.INFORMATION);
+                info.setTitle("Process Running");
+                info.setHeaderText(header);
+                info.setContentText(message);
+                info.getDialogPane().getChildren().remove(info.getDialogPane().lookupButton(ButtonType.OK));
+                info.show();
+           }
+        });
+    }
+    
+    /**
+     * Cancels the shown information dialog.
+     */
+    public void cancelInfoDialog() {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                info.close();
+            }
+        });
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    /**
+    * Checks to see if a specific port is available.
+    *
+    * @return 
+    */
+    public boolean checkPortAvailable() {
+        ServerSocket socket = null;
+        try {
+            socket = new ServerSocket(portNumber);
+            socket.setReuseAddress(true);
+            return true;
+        } catch (IOException e) {
+            Logger.getLogger(SonarQubeHelper.class.getName()).log(Level.SEVERE, null, e);
+        } finally {
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    Logger.getLogger(SonarQubeHelper.class.getName()).log(Level.SEVERE, null, e);
+                }
+            }
+        }
+        return false;
+    }
+    
+    /**
+     * Gets the properies of previous scanned projects.
+     * 
+     * @return Properties
+     */
+    public Properties getPrevProjectProperties() {
+        try {
+            return pHandler.getPrevProjectProperties();
+        } catch (IOException ex) {
+            Logger.getLogger(SonarQubeHelper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        //Won't happen
+        return null;
+    }
+    
+    /**
+     * Creates a new fast-scan project after scanned successfully.
+     * 
+     * @param path 
+     */
+    public void setNewPrevProject(String path) {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    pHandler.setNewPrevProject(path);
+                    mainController.setChoiceBoxModel(getPrevProjectProperties());
+                } catch (IOException ex) {
+                    Logger.getLogger(SonarQubeHelper.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
+    }
+    
+    /**
+     * Lets the method 'updateServerConnection' (in ControllerMain) run on the JavaFX-thread
+     */
+    public void setServerConnectionOn() {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                mainController.updateServerConnection(true);
+            }
+        });
+    }
+}
---
+++ src/models/ScannerTask.java
@@ -0,0 +1,109 @@
+package models;
+
+import guifiles.SonarQubeHelper;
+import java.awt.Desktop;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author David
+ */
+public class ScannerTask implements Runnable {
+
+    private String sqScanner;
+    private String projectPath;
+    private SonarQubeHelper sqHelper;
+    
+    /**
+     * This task runs when the user pressed the 'scan' button.
+     * On this thread the selected project gets scanned.
+     * 
+     * @param sqHelper
+     * @param sqScanner
+     * @param projectPath 
+     */
+    public ScannerTask(SonarQubeHelper sqHelper, String sqScanner, String projectPath) {
+        this.sqScanner = sqScanner;
+        this.projectPath = projectPath;
+        this.sqHelper = sqHelper;
+    }
+    
+    @Override
+    public void run() {
+        if (startSonarScanner()) {
+            openBrowser();
+        } else {
+            sqHelper.cancelInfoDialog();
+            sqHelper.showErrorDialog("Scanning failed");
+        }
+    }
+
+    /**
+     * Starts the scanning of the selected project.
+     * 
+     * @return 
+     */
+    private boolean startSonarScanner() {
+        try {
+            int infoCounter = 0;
+            
+            sqHelper.showInfoDialog("Scanning project", "This can take up to a minute (depends on project size)");
+            
+            //Creating the correct command and executes it
+            String command = "cd " + projectPath + " && " + sqScanner + "\\bin\\sonar-scanner";
+            ProcessBuilder pBuilder = new ProcessBuilder("cmd.exe", "/c", command);
+            Process scanProject = pBuilder.start();
+            
+            //Creating a reader to make sure the command is executed properly before proceding
+            BufferedReader reader = new BufferedReader(new InputStreamReader(scanProject.getInputStream()));
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+                if (line.contains("FAILURE")) {
+                    return false;
+                } else if (line.contains("INFO") && infoCounter == 0) {
+                    infoCounter++;
+                }
+            }
+            
+            //Check the amount of lines containing 'INFO'.
+            //If its 0 means that there has been an exception thrown while sanning the project
+            if (infoCounter == 0) {
+                return false;
+            }
+            
+            //Set new previous scanned project
+            
+            sqHelper.setNewPrevProject(projectPath);
+            
+            //Cancels the dialog shown
+            sqHelper.cancelInfoDialog();
+            return true;
+        }
+        catch (IOException ex) {
+            Logger.getLogger(ScannerTask.class.getName()).log(Level.SEVERE, null, ex);
+            sqHelper.showErrorDialog("Couldn't scan project");
+            return false;
+        }
+    }
+    
+    /**
+     * Opens the supported browser on localhost:9000
+     */
+    private void openBrowser() {
+        if(Desktop.isDesktopSupported()) {
+            try {
+                Desktop.getDesktop().browse(new URI("http://localhost:9000/"));
+            }
+            catch (IOException | URISyntaxException ex) {
+                Logger.getLogger(ScannerTask.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+}
